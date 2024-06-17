3cacf303c49624987a197a8542134deac16a5a5f
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-17 14:08:04 +0200

    fixL rating

--- src/components/Ratings.tsx
+++ src/components/Ratings.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import StarRatingIcon from "./StarRatingIcon";
 
 const Ratings: React.FC = () => {
   return (
     <div className="ratings">
-      <div className="rating">⭐️⭐️⭐️⭐️⭐️ Rated 5 Stars in Reviews</div>
-      <div className="rating">⭐️⭐️⭐️⭐️⭐️ Rated 5 Stars in Report Guru</div>
-      <div className="rating">⭐️⭐️⭐️⭐️⭐️ Rated 5 Stars in BestTech</div>
+      <div className="rating">
+        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in Reviews
+      </div>
+      <div className="rating">
+        {" "}
+        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in Report Guru
+      </div>
+      <div className="rating">
+        {" "}
+        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in BestTech
+      </div>
     </div>
   );
 };
