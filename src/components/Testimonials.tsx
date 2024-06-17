7432045876fcdbf9a03d3daebf80fc9062a8fcf8
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-17 12:28:09 +0200

    feat: change testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,31 +4,66 @@ const Testimonials: React.FC = () => {
   return (
     <div className="testimonials">
       <div className="testimonial">
-        <h3>Colton Smith</h3>
-        <p>Verified Buyer</p>
-        <p>
+        <div className="testimonial-content">
+          <img
+            src="assets/images/image-colton.jpg"
+            alt="Colton Smith"
+            className="testimonial-img"
+          />
+          <div className="testimonial-author">
+            <span className="testimonial-author-name">Colton Smith</span>
+            <span className="testimonial-author-description">
+              Verified Buyer
+            </span>
+          </div>
+        </div>
+        <span className="testimonial-author-comment">
           “We needed the same printed design as the one we had ordered a week
           prior. Not only did they find the original order, but we also received
           it in time. Excellent!”
-        </p>
+        </span>
       </div>
+
       <div className="testimonial">
-        <h3>Irene Roberts</h3>
-        <p>Verified Buyer</p>
-        <p>
+        <div className="testimonial-content">
+          <img
+            src="assets/images/image-irene.jpg"
+            alt="Irene Roberts"
+            className="testimonial-img"
+          />
+          <div className="testimonial-author">
+            <span className="testimonial-author-name">Irene Roberts</span>
+            <span className="testimonial-author-description">
+              Verified Buyer
+            </span>
+          </div>
+        </div>
+        <span className="testimonial-author-comment">
           “Customer service is always excellent and very quick turn around.
           Completely delighted with the simplicity of the purchase and the speed
           of delivery.”
-        </p>
+        </span>
       </div>
+
       <div className="testimonial">
-        <h3>Anne Wallace</h3>
-        <p>Verified Buyer</p>
-        <p>
+        <div className="testimonial-content">
+          <img
+            src="assets/images/image-anne.jpg"
+            alt="Anne Wallace"
+            className="testimonial-img"
+          />
+          <div className="testimonial-author">
+            <span className="testimonial-author-name">Anne Wallace</span>
+            <span className="testimonial-author-description">
+              Verified Buyer
+            </span>
+          </div>
+        </div>
+        <span className="testimonial-author-comment">
           “Put an order with this company and can only praise them for the very
           high standard. Will definitely use them again and recommend them to
           everyone!”
-        </p>
+        </span>
       </div>
     </div>
   );
