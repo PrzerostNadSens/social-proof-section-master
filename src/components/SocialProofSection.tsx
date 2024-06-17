fa914dc78eb12b51d7c52655088241c17c7562b9
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-17 13:31:36 +0200

    feat: add background

--- src/components/SocialProofSection.tsx
+++ src/components/SocialProofSection.tsx
@@ -6,19 +6,23 @@ import Testimonials from "./Testimonials";
 
 const MainSection: React.FC = () => {
   return (
-    <div className="container">
-      <div className="header-and-ratings">
-        <div className="header">
-          <h1>10,000+ of our users love our products.</h1>
-          <p>
-            We only provide great products combined with excellent customer
-            service. See what our satisfied customers are saying about our
-            services.
-          </p>
+    <div className="background-container">
+      <div className="background-top-left"></div>
+      <div className="background-bottom-right"></div>
+      <div className="container">
+        <div className="header-and-ratings">
+          <div className="header">
+            <h1>10,000+ of our users love our products.</h1>
+            <p>
+              We only provide great products combined with excellent customer
+              service. See what our satisfied customers are saying about our
+              services.
+            </p>
+          </div>
+          <Ratings />
         </div>
-        <Ratings />
+        <Testimonials />
       </div>
-      <Testimonials />
     </div>
   );
 };
