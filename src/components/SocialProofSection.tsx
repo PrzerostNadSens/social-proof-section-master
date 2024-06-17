004b9c5fd10e53609a70deb458c3a8ebf917dba9
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-17 12:58:14 +0200

    feat: change header

--- src/components/SocialProofSection.tsx
+++ src/components/SocialProofSection.tsx
@@ -7,15 +7,17 @@ import Testimonials from "./Testimonials";
 const MainSection: React.FC = () => {
   return (
     <div className="container">
-      <div className="header">
-        <h1>10,000+ of our users love our products.</h1>
-        <p>
-          We only provide great products combined with excellent customer
-          service. See what our satisfied customers are saying about our
-          services.
-        </p>
+      <div className="header-and-ratings">
+        <div className="header">
+          <h1>10,000+ of our users love our products.</h1>
+          <p>
+            We only provide great products combined with excellent customer
+            service. See what our satisfied customers are saying about our
+            services.
+          </p>
+        </div>
+        <Ratings />
       </div>
-      <Ratings />
       <Testimonials />
     </div>
   );
