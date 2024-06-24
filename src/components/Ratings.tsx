f05e8c1999f529469eb0ada137a31da11ac13143
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-24 18:49:42 +0200

    fix: add index to rating and testimonial

--- src/components/Ratings.tsx
+++ src/components/Ratings.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import StarRatingIcon from "./StarRatingIcon";
 
+interface RatingData {
+  text: string;
+  numberOfStars: number;
+}
+
+const ratingsData: RatingData[] = [
+  { text: "Rated 5 Stars in Reviews", numberOfStars: 5 },
+  { text: "Rated 5 Stars in Report Guru", numberOfStars: 5 },
+  { text: "Rated 5 Stars in BestTech", numberOfStars: 5 },
+];
+
 const Ratings: React.FC = () => {
   return (
     <div className="ratings">
-      <div className="rating">
-        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in Reviews
-      </div>
-      <div className="rating">
-        {" "}
-        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in Report Guru
-      </div>
-      <div className="rating">
-        {" "}
-        <StarRatingIcon numberOfStars={5} /> Rated 5 Stars in BestTech
-      </div>
+      {ratingsData.map((rating, index) => (
+        <div
+          key={index}
+          className="rating"
+          style={{ "--index": index + 1 } as React.CSSProperties}
+        >
+          <StarRatingIcon numberOfStars={rating.numberOfStars} />
+          {rating.text}
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,70 +1,55 @@
 import React from "react";
 
+const testimonialsData = [
+  {
+    imgSrc: "assets/images/image-colton.jpg",
+    name: "Colton Smith",
+    description: "Verified Buyer",
+    comment: `“We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!”`,
+  },
+  {
+    imgSrc: "assets/images/image-irene.jpg",
+    name: "Irene Roberts",
+    description: "Verified Buyer",
+    comment: `“Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.”`,
+  },
+  {
+    imgSrc: "assets/images/image-anne.jpg",
+    name: "Anne Wallace",
+    description: "Verified Buyer",
+    comment: `“Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!”`,
+  },
+];
+
 const Testimonials: React.FC = () => {
   return (
     <div className="testimonials">
-      <div className="testimonial">
-        <div className="testimonial-content">
-          <img
-            src="assets/images/image-colton.jpg"
-            alt="Colton Smith"
-            className="testimonial-img"
-          />
-          <div className="testimonial-author">
-            <span className="testimonial-author-name">Colton Smith</span>
-            <span className="testimonial-author-description">
-              Verified Buyer
-            </span>
+      {testimonialsData.map((testimonial, index) => (
+        <div
+          key={index}
+          className="testimonial"
+          style={{ "--index": index + 1 } as React.CSSProperties}
+        >
+          <div className="testimonial-content">
+            <img
+              src={testimonial.imgSrc}
+              alt={testimonial.name}
+              className="testimonial-img"
+            />
+            <div className="testimonial-author">
+              <span className="testimonial-author-name">
+                {testimonial.name}
+              </span>
+              <span className="testimonial-author-description">
+                {testimonial.description}
+              </span>
+            </div>
           </div>
+          <span className="testimonial-author-comment">
+            {testimonial.comment}
+          </span>
         </div>
-        <span className="testimonial-author-comment">
-          “We needed the same printed design as the one we had ordered a week
-          prior. Not only did they find the original order, but we also received
-          it in time. Excellent!”
-        </span>
-      </div>
-
-      <div className="testimonial">
-        <div className="testimonial-content">
-          <img
-            src="assets/images/image-irene.jpg"
-            alt="Irene Roberts"
-            className="testimonial-img"
-          />
-          <div className="testimonial-author">
-            <span className="testimonial-author-name">Irene Roberts</span>
-            <span className="testimonial-author-description">
-              Verified Buyer
-            </span>
-          </div>
-        </div>
-        <span className="testimonial-author-comment">
-          “Customer service is always excellent and very quick turn around.
-          Completely delighted with the simplicity of the purchase and the speed
-          of delivery.”
-        </span>
-      </div>
-
-      <div className="testimonial">
-        <div className="testimonial-content">
-          <img
-            src="assets/images/image-anne.jpg"
-            alt="Anne Wallace"
-            className="testimonial-img"
-          />
-          <div className="testimonial-author">
-            <span className="testimonial-author-name">Anne Wallace</span>
-            <span className="testimonial-author-description">
-              Verified Buyer
-            </span>
-          </div>
-        </div>
-        <span className="testimonial-author-comment">
-          “Put an order with this company and can only praise them for the very
-          high standard. Will definitely use them again and recommend them to
-          everyone!”
-        </span>
-      </div>
+      ))}
     </div>
   );
 };
